ec29ef24f9752866b1ab84cc4d322dcb50ca934a
Mate GABRI <mate@gabri.hu>
2017-07-13 22:11:04 +0930

    Initial commit

---
+++ parse.js
@@ -0,0 +1,33 @@
+module.exports = function(validFields) {
+  return function(req, res, next) {
+    // _id is returned every time
+    var projection = {
+      _id: 1
+    };
+
+    var queryFields = req.query.fields || '';
+
+    if (queryFields === '') {
+      return next();
+    }
+
+    var fields = queryFields.split(',');
+
+    fields.forEach(function(field) {
+      // continue if requested field is id
+      if (field === 'id' || !field) {
+        return;
+      }
+
+      if (validFields.indexOf(field) === -1) {
+        return next(new Error('Invalid field'));
+      }
+
+      projection[field] = 1;
+    });
+
+    req._projection = projection;
+
+    return next();
+  };
+};
